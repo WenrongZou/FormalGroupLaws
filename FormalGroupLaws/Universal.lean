9a92c45954fd5132a1f64ede66b0eba977e5e9cb
WenrongZou <wenrongzou@outlook.com>
2026-01-31 00:57:37 +0100

    universe and corepresentable

--- FormalGroupLaws/Universal.lean
+++ FormalGroupLaws/Universal.lean
@@ -6,12 +6,12 @@ universe u v w
 
 noncomputable section Universal
 
-open CategoryTheory Functor
+open CategoryTheory Functor CommRingCat
 
-variable (L : Type*) [CommRing L] (U : FormalGroup L)
+variable (L : Type u) [CommRing L] (U : FormalGroup L) {C : Type u} [Category.{v} C]
   -- (FGL : CommRingCat → Type)
 
-def FGL : CommRingCat ⥤ Type where
+def FGL : CommRingCat.{u} ⥤ Type u where
   obj R := FormalGroup R
   map f := FormalGroup.map f.hom
   map_id R := by
@@ -22,9 +22,35 @@ def FGL : CommRingCat ⥤ Type where
     simp [F.map_comp]
 
 def FormalGroup.IsUniversalOver : Prop :=
-    ∀ (R : Type*) [CommRing R], ∀ F, ∃! (φ : L →+* R), U.map φ = F
+    ∀ (R : Type u) [CommRing R], ∀ F, ∃! (φ : L →+* R), U.map φ = F
 
--- theorem FGL_representable (h : U.IsUniversalOver L) : FGL.IsRepresentedBy U := sorry
+section
+
+/--
+A presheaf `F` is represented by `X` with universal element `x : F.obj X`
+if the natural transformation `F ⟶ yoneda.obj X` induced by `x` is an isomorphism.
+For better universe generality, we state this manually as for every `Y`, the
+induced map `(X ⟶ Y) → F.obj Y` is bijective.
+-/
+@[mk_iff]
+structure CategoryTheory.Functor.IsCorepresentedBy
+    (F : C ⥤ Type w) {X : C} (x : F.obj X) : Prop where
+  map_bijective {Y : C} : Function.Bijective (fun f : X ⟶ Y ↦ F.map f x)
+
+end
+
+theorem FGL_representable (h : U.IsUniversalOver L) : FGL.IsCorepresentedBy U (X := of L) := by
+  refine { map_bijective := ?_ }
+  intro Y
+  rw [Function.Bijective, Function.Injective, Function.Surjective]
+  constructor
+  · intro f g h_fg
+    obtain ⟨φ, hU_f, hU_f'⟩ := h Y (U.map f.hom)
+    refine hom_ext ?_
+    rw [hU_f' f.hom rfl, hU_f' g.hom h_fg.symm]
+  intro F
+  obtain ⟨f, hf₁, hf₂⟩ := h (of Y) F
+  exact ⟨(ofHom f), hf₁⟩
 
 #check IsRepresentedBy
 
