895ec9f5c4e747e03f3e7239edff144eecc62312
WenrongZou <wenrongzou@outlook.com>
2026-03-26 20:51:28 +0100

    update

--- FormalGroupLaws/Height.lean
+++ FormalGroupLaws/Height.lean
@@ -1,3 +1,12 @@
 import FormalGroupLaws.Basic
 
-variable {R : Type*} [CommRing R] (F : FormalGroup R)
+variable {R : Type*} [CommRing R]
+
+open PowerSeries
+
+namespace FormalGroup
+
+noncomputable
+def nseries (F : FormalGroup R) : ℕ → PowerSeries R
+| 0 => 0
+| k + 1 => F.toFun.subst ![(nseries F k), X]
