d51d89ea4ce920fc1a1fffa2e7e0a20a1e1b1603
WenrongZou <wenrongzou@outlook.com>
2026-04-08 11:19:40 +0200

    some proofs in height

--- FormalGroupLaws/Height.lean
+++ FormalGroupLaws/Height.lean
@@ -2,7 +2,7 @@ import FormalGroupLaws.Basic
 
 variable {R : Type*} [CommRing R] {σ τ : Type}
 
-open PowerSeries
+open PowerSeries Finsupp
 
 namespace FormalGroup
 
@@ -28,8 +28,16 @@ lemma nseries_ConstCoeff : (F.nseries n).constantCoeff = 0 := by
 lemma subst_zero_eq_zero {f : MvPowerSeries σ R} (hf : f.constantCoeff = 0) :
     f.subst (0 : σ → MvPowerSeries τ R) = 0 := by
   ext n
-  rw [MvPowerSeries.coeff_subst ((MvPowerSeries.hasSubst_def 0).mpr (by simp))]
-  sorry
+  rw [MvPowerSeries.coeff_subst ((MvPowerSeries.hasSubst_def 0).mpr (by simp)),
+    MvPowerSeries.coeff_zero, finsum_eq_zero_of_forall_eq_zero]
+  intro d
+  by_cases hd : d = 0
+  · simp [hd, hf]
+  · have : (d.prod fun s e ↦ (0 : σ → MvPowerSeries τ R) s ^ e) = 0 := by
+      have ⟨i, hi⟩ : d.support.Nonempty := support_nonempty_iff.mpr hd
+      rw [prod, Finset.prod_eq_zero hi]
+      · rw [Pi.zero_apply, zero_pow (mem_support_iff.mp hi)]
+    rw [this, MvPowerSeries.coeff_zero, smul_zero]
 
 noncomputable
 def _root_.FormalGroupHom.nseries : FormalGroupHom F F where
@@ -42,7 +50,23 @@ def _root_.FormalGroupHom.nseries : FormalGroupHom F F where
       rw [← coe_substAlgHom <| HasSubst.of_constantCoeff_zero F.zero_constantCoeff,
         map_zero, ← Pi.zero_def,  subst_zero_eq_zero F.zero_constantCoeff]
     | succ k ih =>
-      sorry
+      calc
+        _ = F.toFun.subst ![(F.nseries k).subst F.toFun, F.toFun] := sorry
+        _ = (F.nseries k).subst F.toFun +[F] F.toFun := rfl
+        _ = (F.nseries k).toMvPowerSeries (0 : Fin 2) +[F] (F.nseries k).toMvPowerSeries 1 +[F]
+          (MvPowerSeries.X 1 +[F] MvPowerSeries.X 0) := by
+          simp_rw [ih]
+          congr
+          · exact List.ofFn_inj.mp rfl
+          · -- easy sorry, use comm
+            sorry
+        _ = (F.nseries k).toMvPowerSeries (0 : Fin 2) +[F] ((F.nseries k).toMvPowerSeries 1 +[F]
+            MvPowerSeries.X 1) +[F] MvPowerSeries.X 0 := sorry
+        _ = (F.nseries k).toMvPowerSeries (0 : Fin 2) +[F] MvPowerSeries.X 0
+            +[F] (F.nseries (k + 1)).toMvPowerSeries 1 := sorry
+        _ = _ := by
+
+          sorry
 
 section Height
 
@@ -50,11 +74,13 @@ variable {p : ℕ} [ExpChar R p] {hp : p ≠ 1}
 
 lemma zero_of_coeff_prime_pow_zero :
     (∀ n, coeff (p ^ n) (F.nseries p) = 0) → F.nseries p = 0 := by
+
   sorry
 
 theorem exist_coeff_pow_ne_zero_iff_ne_zero :
-    (∃ n, coeff (p ^ n) (F.nseries p) ≠ 0) ↔ F.nseries p ≠ 0 := by
-  sorry
+    (∃ n, coeff (p ^ n) (F.nseries p) ≠ 0) ↔ F.nseries p ≠ 0 :=
+  ⟨fun ⟨n, hn⟩ => ne_of_apply_ne (coeff (p ^ n)) (hn · ),
+    by contrapose!; exact zero_of_coeff_prime_pow_zero _⟩
 
 noncomputable
 def height : ℕ∞ :=
