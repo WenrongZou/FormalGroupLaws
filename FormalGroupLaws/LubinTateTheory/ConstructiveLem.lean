9b5c689678650268df4f52d99cb76738f16f36c1
WenrongZou <wenrongzou@outlook.com>
2026-05-09 17:47:44 +0200

    some update on LubinTate

--- FormalGroupLaws/LubinTateTheory/ConstructiveLem.lean
+++ FormalGroupLaws/LubinTateTheory/ConstructiveLem.lean
@@ -9,125 +9,148 @@ public import Mathlib.RingTheory.MvPowerSeries.Trunc
 
 noncomputable section
 
-namespace LubinTate
-
 open ValuativeRel MvPowerSeries Classical
 
-variable {K : Type*} [Field K] [ValuativeRel K] [UniformSpace K] [IsUniformAddGroup K]
-  [IsValuativeTopology K] [IsNonarchimedeanLocalField K]
+variable {K σ : Type*} [Field K] [ValuativeRel K] [TopologicalSpace K]
+  [IsNonarchimedeanLocalField K] (π : (valuation K).Uniformizer)
+
+namespace FormalGroup.LubinTate
+
+-- variable (K : Type*) [Field K] [ValuativeRel K] [TopologicalSpace K] [IsNonarchimedeanLocalField K]
 
 /-- For a nonarchimedean local field, we have that valuation of `K` is
 rank one discrete. -/
 instance : (valuation K).IsRankOneDiscrete := inferInstance
 
-/- `π` is a uniformizer of `valuation K`. -/
-variable (π : 𝒪[K]) (h : (valuation K).IsUniformizer π)
-
 instance : Fintype 𝓀[K] := Fintype.ofFinite 𝓀[K]
 
 structure 𝓕 where
   toPowerSeries : PowerSeries 𝒪[K]
-  trunc_degree_two : toPowerSeries.trunc 2 = Polynomial.C π * Polynomial.X
-  mod_pi : PowerSeries.C π ∣ toPowerSeries - PowerSeries.X ^ Fintype.card 𝓀[K]
+  trunc_two : toPowerSeries.trunc 2 = Polynomial.C π.val * Polynomial.X
+  pi_dvd : PowerSeries.C π.val ∣ toPowerSeries - PowerSeries.X ^ Fintype.card 𝓀[K]
+
+/-- Constant coefficient fo `f : 𝓕 π` is zero. -/
+lemma constanceCoeff_F {f : 𝓕 π} : f.toPowerSeries.constantCoeff = 0 := by
+  calc
+    _ = (f.toPowerSeries.trunc 2).constantCoeff := by
+      simp [Polynomial.constantCoeff_apply, PowerSeries.coeff_trunc]
+    _ = _ := by simp [f.trunc_two]
+
+end LubinTate
+
+end FormalGroup
+
+namespace MvPowerSeries
+
+open FormalGroup LubinTate
+
+variable {R σ : Type*} [CommRing R]
+
+lemma C_dvd_iff_forall_dvd_coeff (c : R) (p : MvPowerSeries σ R) :
+  C c ∣ p ↔ ∀ n, c ∣ (coeff n) p := by
+  constructor <;> intro hp
+  · intro n
+    obtain ⟨d, rfl⟩ := hp
+    simp
+  · use fun n ↦ Classical.choose (hp n)
+    ext n
+    simp
+    rw [Classical.choose_spec (hp n)]
+    rfl
+
+lemma dvd_prod_pow_sub_prod_pow_of_dvd_sub {d : R} {a b : σ → R}
+    (h : ∀ i : σ, d ∣ a i - b i)
+    (i : σ →₀ ℕ) :
+    d ∣ (i.prod fun j e ↦ a j ^ e) - (i.prod fun j e ↦ b j ^ e) := by
+  induction i using Finsupp.induction₂ with
+  | zero => simp
+  | add_single i e f ha hb₀ ih =>
+    classical
+    rw [Finsupp.prod_add_index_of_disjoint, Finsupp.prod_add_index_of_disjoint,
+      Finsupp.prod_single_index, Finsupp.prod_single_index]
+    · obtain ⟨q, hq⟩ := ih
+      rw [sub_eq_iff_eq_add] at hq
+      rw [hq, add_mul, add_sub_assoc, ← mul_sub, mul_assoc]
+      apply dvd_add (dvd_mul_right _ _) (dvd_mul_of_dvd_right _ _)
+      exact (h i).trans (sub_dvd_pow_sub_pow ..)
+    · simp
+    · simp
+    · apply Finset.disjoint_iff_inter_eq_empty.mpr
+      ext w
+      simp [Finsupp.single, hb₀]
+      contrapose!
+      rintro rfl
+      simpa using ha
+    · apply Finset.disjoint_iff_inter_eq_empty.mpr
+      ext w
+      simp [Finsupp.single, hb₀]
+      contrapose!
+      rintro rfl
+      simpa using ha
+
+end MvPowerSeries
+
+namespace FormalGroup
+
+namespace LubinTate
+
+instance Invertible.pi_sub_pi_pow {k : ℕ} [NeZero k] :
+    Invertible (1 - π.val ^ k) := by
+  have : π.val ^ k ∈ 𝓂[K] := by
+    simp [Ideal.pow_mem_of_mem, π.valuation_gt_one.not_isUnit, Nat.pos_of_neZero]
+  exact IsUnit.invertible <| IsLocalRing.isUnit_one_sub_self_of_mem_nonunits _ this
+
+-- variable {k : ℕ} [NeZero k] in
+-- #check  ⅟(1 - π.val ^ k)
+
+instance [Finite σ] : Fintype σ := Fintype.ofFinite σ
+
+open Finset
+
+variable [Finite σ] {L : MvPolynomial σ 𝒪[K]} (f g : 𝓕 π)
+  (hL : ∀ i ∈ L.support, Finset.univ.sum i = 1)
+
+-- variable (π f g) in
+def Phi_aux (hL : ∀ i ∈ L.support, Finset.univ.sum i = 1) : ℕ → MvPowerSeries σ 𝒪[K]
+  | 0 => 0
+  | 1 => L
+  | n + 1 =>
+    have : C π.val * C (1 - π.val ^ n) ∣ (Phi_aux hL n).subst (g.toPowerSeries.toMvPowerSeries · )
+      - f.toPowerSeries.subst (Phi_aux hL n):= sorry
+    Phi_aux hL n + Exists.choose this
+
+/-- This is a unique solution in the constructive lemma. -/
+def Phi : MvPowerSeries σ 𝒪[K] := fun d => (Phi_aux π f g hL d.degree).coeff d
+
+lemma constructive_lemma : f.toPowerSeries.subst (Phi π f g hL) =
+  (Phi π f g hL).subst (g.toPowerSeries.toMvPowerSeries · ) := sorry
+
+lemma constructive_lemma_ind_hyp
+    (n : ℕ) {ϕ₁ : MvPolynomial (Fin n) 𝒪[K]}
+    (hϕ₁ : ∀ i ∈ ϕ₁.support, Finset.univ.sum i = 1)
+    {a : Fin n → 𝒪[K]} (f g : 𝓕 π) (r : ℕ) (hr : 2 ≤ r) :
+    ∃! ϕr : MvPolynomial (Fin n) 𝒪[K], ϕr.totalDegree < r
+        ∧ truncTotal 2 ϕr = ϕ₁
+          ∧ truncTotal r (f.toPowerSeries.subst ϕr.toMvPowerSeries)
+            = truncTotal r (ϕr.toMvPowerSeries.subst (g.toPowerSeries.toMvPowerSeries ·)) := by
+  induction r, hr using Nat.le_induction with
+  | base => sorry
+  | succ d hd ih =>
+
+    sorry
 
 
+-- theorem constructive_lemma
+--     (n : ℕ) {ϕ₁ : MvPolynomial (Fin n) 𝒪[K]}
+--     (h_ϕ₁ : ∀ i ∈ ϕ₁.support, Finset.univ.sum i = 1)
+--     (f g : 𝓕 π) :
+--     ∃! ϕ : MvPowerSeries (Fin n) 𝒪[K],
+--       truncTotal 2 ϕ = ϕ₁
+--         ∧ PowerSeries.subst ϕ f.toPowerSeries = subst (g.toPowerSeries.toMvPowerSeries ·) ϕ := by
 
-
--- universe u
-
--- variable {K : Type u} [Field K] [ValuativeRel K] [UniformSpace K] [IsUniformAddGroup K]
---   [IsValuativeTopology K] [IsNonarchimedeanLocalField K] (π : 𝒪[K])
-
--- instance : IsCyclic (ValueGroupWithZero K)ˣ :=
---   (Units.mapEquiv
---     (IsNonarchimedeanLocalField.valueGroupWithZeroIsoInt K).toMulEquiv).isCyclic.mpr
---     inferInstance
-
--- instance : Nontrivial (ValueGroupWithZero K)ˣ :=
---   ValuativeRel.isNontrivial_iff_nontrivial_units.mp inferInstance
-
--- variable (hπ : (Valued.v (R := K)).IsUniformizer π)
-
--- noncomputable section LubinTateF
-
--- instance : Fintype 𝓀[K] := Fintype.ofFinite 𝓀[K]
-
--- structure LubinTateF where
---   toPowerSeries : PowerSeries 𝒪[K]
---   trunc_degree_two : toPowerSeries.trunc 2 = Polynomial.C π * Polynomial.X
---   mod_pi : PowerSeries.C π ∣ toPowerSeries - PowerSeries.X ^ Fintype.card 𝓀[K]
--- namespace LubinTateF
-
--- variable (F : LubinTateF π)
-
--- lemma toMvPowerSeries_trunc_degree_two :
---     (F.toPowerSeries : MvPowerSeries Unit 𝒪[K]).truncTotal _ 2
---       = MvPolynomial.C π * MvPolynomial.X default := by
 --   sorry
---   -- rw [truncTotalDeg_powerSeries, (MvPolynomial.pUnitAlgEquiv _).symm_apply_eq]
---   -- simpa using F.trunc_degree_two
-
--- lemma toMvPowerSeries_mod_pi :
---     MvPowerSeries.C  π ∣ F.toPowerSeries - MvPowerSeries.X default ^ Fintype.card 𝓀[K] :=
---   F.mod_pi
-
--- /-- constant coefficient of `f` in Lubin Tate `F_π` is zero.-/
--- lemma constantCoeff_LubinTateF : PowerSeries.constantCoeff F.toPowerSeries = 0 := by
---   sorry
 
 
--- end LubinTateF
-
--- end LubinTateF
-
--- noncomputable section
-
--- variable {σ : Type*} {R : Type*} [CommRing R] {τ : Type*}
-
--- section Prop_2_11
-
--- namespace MvPowerSeries
-
--- lemma C_dvd_iff_forall_dvd_coeff (c : R) (p : MvPowerSeries σ R) :
---   C c ∣ p ↔ ∀ n, c ∣ (coeff n) p := by
---   constructor <;> intro hp
---   · intro n
---     obtain ⟨d, rfl⟩ := hp
---     simp
---   · use fun n ↦ Classical.choose (hp n)
---     ext n
---     simp
---     rw [Classical.choose_spec (hp n)]
---     rfl
-
--- lemma dvd_prod_pow_sub_prod_pow_of_dvd_sub {d : R} {a b : σ → R}
---     (h : ∀ i : σ, d ∣ a i - b i)
---     (i : σ →₀ ℕ) :
---     d ∣ (i.prod fun j e ↦ a j ^ e) - (i.prod fun j e ↦ b j ^ e) := by
---   induction i using Finsupp.induction₂ with
---   | zero => simp
---   | add_single i e f ha hb₀ ih =>
---     rw [Finsupp.prod_add_index_of_disjoint, Finsupp.prod_add_index_of_disjoint,
---       Finsupp.prod_single_index, Finsupp.prod_single_index]
---     · obtain ⟨q, hq⟩ := ih
---       rw [sub_eq_iff_eq_add] at hq
---       rw [hq, add_mul, add_sub_assoc, ← mul_sub, mul_assoc]
---       apply dvd_add (dvd_mul_right _ _) (dvd_mul_of_dvd_right _ _)
---       exact (h i).trans (sub_dvd_pow_sub_pow ..)
---     · simp
---     · simp
---     · apply Finset.disjoint_iff_inter_eq_empty.mpr
---       ext w
---       simp [Finsupp.single, hb₀]
---       contrapose!
---       rintro rfl
---       simpa using ha
---     · apply Finset.disjoint_iff_inter_eq_empty.mpr
---       ext w
---       simp [Finsupp.single, hb₀]
---       contrapose!
---       rintro rfl
---       simpa using ha
 
 -- variable [DecidableEq σ] [Fintype σ] [DecidableEq τ] [Fintype τ]
 
